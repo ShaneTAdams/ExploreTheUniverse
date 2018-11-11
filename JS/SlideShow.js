13351fd40775f4815545c9c672d052bce903c89f
ShaneTAdams <shane.t.adams@gmail.com>
2018-11-11 14:44:05 -0500

    Updates to CSS files.

--- JS/SlideShow.js
+++ JS/SlideShow.js
@@ -1,28 +1,27 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var myslideshow = 1;
+showSlides(myslideshow);
 
-// Next/previous controls
+
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides(myslideshow += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides(myslideshow = n);
 }
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {myslideshow = 1}
+  if (n < 1) {myslideshow = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[myslideshow-1].style.display = "block";
+  dots[myslideshow-1].className += " active";
 }
